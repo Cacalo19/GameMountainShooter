57c5ded4a2a62743092f8241737e78228849fbfa
Cacalo19 <carlosfeitoza17@gmail.com>
2026-02-20 13:59:47 -0300

    Criado Asset folder, Inserido todos assets, criando o menu: backgrund, som e fontes

--- code/Menu.py
+++ code/Menu.py
@@ -1,6 +1,40 @@
+import pygame.image
+
+from pygame import Surface, Rect
+from pygame.font import Font
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Const import color_orange, color_white, MENU_OPTION
+
+
 class Menu:
     def __init__(self, window):
-        self.windows = window
+        self.window = window
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.rect = self.surf.get_rect()
 
-    def run(self):
-        pass
+
+    def run(self, ):        
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, 'Mountain', (color_orange), (WIN_WIDTH / 2, 70))
+            self.menu_text(50, 'Shooter', (color_orange), (WIN_WIDTH / 2, 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], (color_white), (WIN_WIDTH / 2, 200 + i * 25))
+            
+            pygame.display.flip()
+            
+            # Check for all events
+            for event in pygame.event.get():
+                # If the user clicks the close button, quit the game
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name= 'Lucida Sans Typewriter', size= text_size)
+        text_surf: Surface = text_font.render(text, antialias=True, color=text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
